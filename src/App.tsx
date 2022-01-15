565a8ccf1b14e8b8aaebef4358e52ad092d77f5f
jmqm <jmqm@outlook.com>
2022-01-14 20:57:51 -0600

    Removed eas stuff, changed scripts accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// Normally this would be root component, but EAS build does not support
-// 'entrypoint' key as of 2022-01-04.
-// https://docs.expo.dev/build-reference/migrating/#custom--main--entry-point-in
-
-// import { registerRootComponent } from "expo";
+import { registerRootComponent } from "expo";
 import Main from "src/screens/Main";
 import Providers from "src/providers/Providers";
 
@@ -14,5 +10,4 @@ const App = () => {
     );
 };
 
-// registerRootComponent(App);
-export default App;
+registerRootComponent(App);
